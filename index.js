39459e5765b676326ceb887d42c641cca4bfe290
Miles Higbie <miles.higbie@gmail.com>
2020-02-19 14:06:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-document.addEventListener("DOMContentLoaded", const f = () => {
+document.addEventListener("DOMContentLoaded", f => {
   let t = document.querySelector("#text");
   t.textContent = "This is really cool!"
 });
